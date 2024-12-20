0c35731bae46b5cc758ce55662d974bf54f768aa
Lucas Gomes <lucas@careintelligence.ai>
2024-12-20 03:02:41 -0300

    Appbar fixed

--- src/components/AppBar/index.js
+++ src/components/AppBar/index.js
@@ -1,8 +1,11 @@
 import LogoSVG from "../Logo";
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next"; // Importando o hook de tradução
 import "./styles.css"
 
 function Navbar() {
+    const { t } = useTranslation(); // Hook para acessar a tradução
+
     return (
         <nav>
             <Link to={"/"}>
@@ -10,8 +13,8 @@ function Navbar() {
             </Link>
             <div className="nav-content">
                 <Link to={"/skills"}>
-                    <button>
-                        Habilidades
+                <button>
+                        {t('navbar.skills')} {/* Texto traduzido */}
                     </button>
                 </Link>
             </div>
